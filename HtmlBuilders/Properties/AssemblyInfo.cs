248b15e032eb512641a1451127d1367cbc6ca433
Christian Pfahl (KriNiTo) <KriNiTo@users.noreply.github.com>
2015-07-10 13:01:27 +0200

    Added .nuspec and .cmd-file to push "HtmlBuilders-MVC4-Backport" to NuGet.org

--- HtmlBuilders/Properties/AssemblyInfo.cs
+++ HtmlBuilders/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("HtmlBuilders")]
+[assembly: AssemblyTitle("HtmlBuilders-MVC4-Backport")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Alexander Moerman, Christian Pfahl")]
 [assembly: AssemblyProduct("HtmlBuilders")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
@@ -36,6 +36,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyInformationalVersion("1.0.2")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
 [assembly: InternalsVisibleTo("HtmlBuilders.Tests")]
